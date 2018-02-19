8f0d13c026b92826476647a69c8f73694bd49e64
Benjamin Fox <deadalus.ai@gmail.com>
2018-02-19 18:06:09 +1300

    Working example

--- src/app/example1/index.ts
+++ src/app/example1/index.ts
@@ -1,18 +1,22 @@
 import { Component, NgModule } from "@angular/core";
 import { StickyModule } from "../sticky";
+import { TimestampComponent } from "./timestamp";
+import { CommonModule } from "@angular/common";
 
 @Component({
     templateUrl: './template.html'
 })
 export class Example1Component {
-
+    show = false;
 }
 
 @NgModule({
     imports: [
+        CommonModule,
         StickyModule
     ],
     declarations: [
+        TimestampComponent,
         Example1Component
     ],
     exports: [
